f26efb0b9be870650b6cb4bc8403594e63a724cd
Rik <gitaarik@posteo.net>
2020-12-21 18:46:13 -0500

    using LitStyle for demo-component and demo-page

--- src/demo-component.js
+++ src/demo-component.js
@@ -1,61 +1,44 @@
 import { LitElement, css } from 'lit-element';
+import { litStyle } from 'lit-element-style';
 
 
-export const DemoComponent = superclass => class extends superclass {
+export const DemoComponent = litStyle(css`
 
-    static getStyles() {
-        if (!this.styles) {
-            return this._defaultStyles;
-        } else if (Array.isArray(this.styles)) {
-            return [this._defaultStyles, ...this.styles];
-        } else {
-            return [this._defaultStyles, this.styles];
-        }
+    :host {
+        display: block;
+        padding: 15px;
+        background: #DAD7D2;
+        border: 1px #666 solid;
     }
 
-    static get _defaultStyles() {
-
-        return css`
-
-            :host {
-                display: block;
-                padding: 15px;
-                background: #DAD7D2;
-                border: 1px #666 solid;
-            }
-
-            h2 {
-                margin-top: 0;
-                font-size: 20px;
-                color: green;
-            }
-
-            h3 {
-                margin: 20px 0;
-                font-weight: 600;
-                font-size: 16px;
-            }
-
-            .status {
-                color: blue;
-            }
-
-            .value {
-                color: red;
-            }
-
-            .buttons {
-                display: flex;
-                flex-wrap: wrap;
-                margin: -5px 0 0 -5px;
-            }
-
-            .buttons > * {
-                margin: 5px 0 0 5px;
-            }
-
-        `;
-
+    h2 {
+        margin-top: 0;
+        font-size: 20px;
+        color: green;
     }
 
-}
+    h3 {
+        margin: 20px 0;
+        font-weight: 600;
+        font-size: 16px;
+    }
+
+    .status {
+        color: blue;
+    }
+
+    .value {
+        color: red;
+    }
+
+    .buttons {
+        display: flex;
+        flex-wrap: wrap;
+        margin: -5px 0 0 -5px;
+    }
+
+    .buttons > * {
+        margin: 5px 0 0 5px;
+    }
+
+`);
--- src/demo-page.js
+++ src/demo-page.js
@@ -1,69 +1,52 @@
 import { css } from 'lit-element';
+import { litStyle } from 'lit-element-style';
 
 
-export const DemoPage = superclass => class extends superclass {
+export const DemoPage = litStyle(css`
 
-    static getStyles() {
-        if (!this.styles) {
-            return this._defaultStyles;
-        } else if (Array.isArray(this.styles)) {
-            return [this._defaultStyles, ...this.styles];
-        } else {
-            return [this._defaultStyles, this.styles];
-        }
+    :host {
+        display: block;
     }
 
-    static get _defaultStyles() {
-
-        return css`
-
-            :host {
-                display: block;
-            }
-
-            * {
-                box-sizing: border-box;
-            }
-
-            h1 {
-                padding: 10px 0;
-                margin: 20px 0 15px;
-                font-size: 25px;
-                border-bottom: 1px solid #AAA;
-            }
-
-            h2 {
-                margin: 25px 0 10px;
-                font-size: 20px;
-            }
-
-            h3 {
-                margin: 20px 0 5px;
-                font-size: 16px;
-            }
-
-            p {
-                margin: 15px 0;
-                line-height: 1.5;
-            }
-
-            a {
-                color: #000;
-            }
-
-            .demoComponents {
-                display: flex;
-                flex-wrap: wrap;
-                margin: -15px 0 0 -15px;
-            }
-
-            .demoComponents > * {
-                margin: 15px 0 0 15px;
-                max-width: 290px;
-            }
-
-        `;
-
+    * {
+        box-sizing: border-box;
     }
 
-}
+    h1 {
+        padding: 10px 0;
+        margin: 20px 0 15px;
+        font-size: 25px;
+        border-bottom: 1px solid #AAA;
+    }
+
+    h2 {
+        margin: 25px 0 10px;
+        font-size: 20px;
+    }
+
+    h3 {
+        margin: 20px 0 5px;
+        font-size: 16px;
+    }
+
+    p {
+        margin: 15px 0;
+        line-height: 1.5;
+    }
+
+    a {
+        color: #000;
+    }
+
+    .demoComponents {
+        display: flex;
+        flex-wrap: wrap;
+        margin: -15px 0 0 -15px;
+    }
+
+    .demoComponents > * {
+        margin: 15px 0 0 15px;
+        max-width: 290px;
+    }
+
+`);
