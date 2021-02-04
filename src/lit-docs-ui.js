ea54e9836736c3b640b5754d45443176a8496548
Rik <gitaarik@posteo.net>
2021-02-03 23:06:34 -0500

    lit-docs-ui css fix

--- src/lit-docs-ui.js
+++ src/lit-docs-ui.js
@@ -429,6 +429,7 @@ class LitDocsUI extends observeState(LitDocsStyle(LitElement)) {
 
             article {
                 flex-grow: 1;
+                max-width: 100%;
             }
 
             #articleContent {
