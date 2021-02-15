258d69154b2fc4a4de8e6692548e27578a8ea02f
Rik <gitaarik@posteo.net>
2021-02-14 21:15:54 -0500

    removed old undercore dirs

--- docs/build/_dist_/helpers/index.js
+++
@@ -1,5 +0,0 @@
-import './code-block.js';
-import './lit-docs-ui.js';
-import { DemoComponent } from './demo-component.js';
-import { LitDocsStyle } from './lit-docs-style.js';
-export { DemoComponent, LitDocsStyle };
--- docs/build/_dist_/lit-docs/lit-docs-content.js
+++
@@ -1,3 +0,0 @@
-import { LitDocsAnchors } from './lit-docs-anchors.js';
-import { LitDocsStyle } from './lit-docs-style.js';
-export const LitDocsContent = superclass => class extends LitDocsAnchors(LitDocsStyle(superclass)) {};
